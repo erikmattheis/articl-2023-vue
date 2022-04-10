e9500f5faeae374993728bfd5c5c768276b64de3
erikmattheis <gozz@gozz.com>
2022-04-10 02:21:43 -0500

    Add all vue

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,86 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <the-header></the-header>
+  <main>
+    <router-view v-slot="{ Component }">
+      <transition name="scale" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
+  </main>
+  <the-footer></the-footer>
+  <modal-error></modal-error>
 </template>
 
+<style lang="css">
+body {
+  border-radius: 0;
+  font-size: 12px;
+}
+</style>
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ModalError from '@/components/ui/ModalError.vue';
+import TheHeader from '@/components/layout/TheHeader.vue';
+import TheFooter from '@/components/layout/TheFooter.vue';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    TheHeader,
+    TheFooter,
+    ModalError,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import '~/node_modules/normalize.css/normalize.css';
+@import '~/node_modules/the-new-css-reset/css/reset.css';
+@import '~/node_modules/@picocss/pico/css/pico.css';
+
+@import url('//fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+body,
+input:-webkit-autofill::first-line,
+input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+input:-webkit-autofill:active {
+  font: 17px/1.3 'Poppins', sans-serif !important;
+}
+
+body * {
+  border-radius: 0 !important;
+}
+
+.lighter {
+  opacity: 0.7;
+}
+
+.left-space {
+  padding-left: 2rem;
+}
+
+.right {
+  text-align: right;
+}
+
+.error {
+  padding: 1rem;
+  background-color: #ffbaba;
+}
+
+.error li {
+  color: #d8000c;
+}
+
+.scale-enter-active,
+.scale-leave-active {
+  transition: all 0.1s ease;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+  opacity: 0;
+  transform: scaleY(0.9);
 }
 </style>
