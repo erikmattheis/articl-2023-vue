7e297d1db1825056b4709fbd01a3d09b90902b07
erikmattheis <gozz@gozz.com>
2022-06-22 01:22:11 -0500

    stop

--- src/components/layout/ArticlePlaceholder.vue
+++ src/components/layout/ArticlePlaceholder.vue
@@ -1,18 +1,18 @@
 <template>
   <article>
     <div class="box-placeholder">
-      <h2 class="text line">
+      <h2 class="text-p line">
         r&nbsp;
       </h2>
 
       <ul class="nav-tabs">
-        <li class="text line link">
+        <li class="text-p line link">
 &nbsp;
         </li>
-        <li class="text line">
+        <li class="text-p line">
 &nbsp;
         </li>
-        <li class="text">
+        <li class="text-p">
 &nbsp;
         </li>
       </ul>
@@ -37,6 +37,7 @@ export default {
 </script>
 
 <style scoped>
+h2,
 li {
   height: 1rem;
   text-indent:-999rem;
