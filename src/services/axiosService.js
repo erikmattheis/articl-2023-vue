b219cbce888f55c8bf38c3d1abdc65f05db10ab6
erikmattheis <gozz@gozz.com>
2023-01-27 15:42:46 -0600

    Deploy with index.js

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -6,10 +6,7 @@ if (window.location.hostname === '192.168.1.130' || window.location.hostname ===
   baseURL = 'http://localhost:5000/v1';
 } else if (window.location.hostname === 'articl-vue-2022.herokuapp.com') {
   baseURL = 'https://articl-api-dev.herokuapp.com/v1';
-} else {
-  baseURL = 'https://api.articl.net/v1';
 }
-
 export default axios.create({
   baseURL,
 });
