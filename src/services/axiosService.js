4faec7eeb486aabe2bc1ad18a3ac72198768c44e
erikmattheis <gozz@gozz.com>
2023-01-27 22:00:36 -0600

    Use correct API domain

--- src/services/axiosService.js
+++ src/services/axiosService.js
@@ -5,7 +5,7 @@ let baseURL = '';
 if (window.location.hostname === '192.168.1.130' || window.location.hostname === 'localhost') {
   baseURL = 'http://localhost:5000/v1';
 } else if (window.location.hostname === 'articl-vue-2022.herokuapp.com') {
-  baseURL = 'https://articl-api-dev.herokuapp.com/v1';
+  baseURL = 'https://articl-restful-api.herokuapp.com/v1';
 }
 export default axios.create({
   baseURL,
